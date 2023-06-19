9ca4524d81448e2d095321569a910f03016cb92c
M Rayhan Rohadi <110401509+muhammadrayhanr@users.noreply.github.com>
2023-06-19 17:19:52 +0700

    Update index.js untuk mengatasi CORS

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ import dalleRoutes from './routes/dalle.routes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  })
+);
 app.use(express.json({ limig: "50mb" }))
 
 app.use("/api/v1/dalle", dalleRoutes);
